7c2424c59efe0b0356e20cd65b45e8a3bc4ea679
unknown <ariaworkshopnft@gmail.com>
2024-05-31 21:55:42 +0200

    components frontend

--- packages/nextjs/components/AssetWithAmount.tsx
+++ packages/nextjs/components/AssetWithAmount.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const AssetWithAmount = ({imgSrc, amount, name}: any) => {
   return (
     <div className='flex'>
-        <Image src={imgSrc} alt="logo" />
+        <Image src={imgSrc} alt="logo" className='rounded-full' width={100} height={100}  />
         <div className='grid grid-cols-1'>
             <p>{amount}</p>
             <p>{name}</p>
--- packages/nextjs/components/CampaignPageComponent.tsx
+++ packages/nextjs/components/CampaignPageComponent.tsx
@@ -5,13 +5,13 @@ import CoinsDonationComponent from './CoinsDonationComponent'
 
 const CampaignPageComponent = () => {
   return (
-    <>
+    <div className='grid grid-cols-2'>
     <CardWithProgress  imgSrc="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
              title="Fundraiser title"
              description="description goes here..." />
      
     <CoinsDonationComponent />
-  </>
+  </div>
   )
 }
 
