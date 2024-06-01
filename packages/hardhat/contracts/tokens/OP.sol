264048d84237abc59929fe384f4de0741314ec9e
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-06-01 09:12:26 +0200

    reworked token contracts

--- packages/hardhat/contracts/tokens/OP.sol
+++ packages/hardhat/contracts/tokens/OP.sol
@@ -5,12 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, Ownable, ERC20Permit {
+contract OP is ERC20, Ownable, ERC20Permit {
 	constructor(
-		string memory tokenName,
-		string memory ticker,
 		uint256 initialSupply
-	) ERC20(tokenName, ticker) ERC20Permit("MyToken") {
+	) ERC20("Optimism", "OP") ERC20Permit("Optimism") {
 		_mint(msg.sender, initialSupply);
 	}
 
--- packages/hardhat/deploy/01_deploy_your_tokens.ts
+++ packages/hardhat/deploy/01_deploy_your_tokens.ts
@@ -2,30 +2,32 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { Contract } from "ethers";
 
-
 const deployTokens: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
-  const initialSupply = "1000000000000000000000"; // Initial supply for each token
+  const initialSupply = "1000000000000000000000000"; // Initial supply for each token
 
   const tokenDetails = [
     { name: "USDC", ticker: "USDC" },
-    { name: "DogeCoin", ticker: "DOGE" },
-    { name: "Pepe", ticker: "PEPE" },
-    { name: "HarryPotterObamaSonic10Inu", ticker: "BITCOIN" },
+    // { name: "DogeCoin", ticker: "DOGE" },
+    // { name: "Pepe", ticker: "PEPE" },
+    // { name: "HarryPotterObamaSonic10Inu", ticker: "BITCOIN" },
     { name: "Optimism", ticker: "OP" },
     { name: "Mantle", ticker: "MNT" },
     { name: "Polygon", ticker: "MATIC" },
-    { name: "The Graph", ticker: "GRT" },
+    { name: "TheGraph", ticker: "GRT" },
     { name: "EigenLayer", ticker: "EIGEN" },
     { name: "Uniswap", ticker: "UNI" },
     { name: "Zircuit", ticker: "ZRC" },
   ];
 
+  let approvalTexts = []
+
   for (const token of tokenDetails) {
-    const deployment = await deploy("MyToken", {
+    const deployment = await deploy(token.ticker, {
+      contract: `contracts/tokens/${token.ticker}.sol:${token.ticker}`,
       from: deployer,
-      args: [token.name, token.ticker, initialSupply],
+      args: [initialSupply],
       log: true,
       autoMine: true,
     });
@@ -33,13 +35,12 @@ const deployTokens: DeployFunction = async function (hre: HardhatRuntimeEnvironm
     const contractAddress = deployment.address;
     const network = hre.network.name;
 
-    const tokenContract = await hre.ethers.getContract<Contract>("MyToken", deployer);
-    await tokenContract.approve("0x4a2B33A77de8F69b8Cf913aafc6357f4Ce176105",99999999999999999999999999999);
+    const tokenContract = await hre.ethers.getContractAt(token.ticker, contractAddress, deployer);
+    // await tokenContract.approve("0x4a2B33A77de8F69b8Cf913aafc6357f4Ce176105", "99999999999999999999999999999");
+    // TODO: create uniswap pool
 
-
-    console.log(`Deployed ${token.name} (${token.ticker}) at ${contractAddress} on ${network}`);
-    console.log(`yarn hardhat verify "${contractAddress}" --network ${network} "${token.name}" "${token.ticker}" "${initialSupply}"`);
-
+    console.log(`Deployed ${token.ticker}  at ${contractAddress} on ${network}`);
+    console.log(`yarn hardhat verify "${contractAddress}" --network ${network} "${initialSupply}"`);
   }
 };
 
